cd1e04e688818a0ab2b55110b9909a20afcb6683
levi-devpanel <109947881+levidevpanel@users.noreply.github.com>
2025-08-19 09:13:21 +0000

    Add Laravel folder

---
+++ laravel_v2/create_quickstart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ---------------------------------------------------------------------
+# Copyright (C) 2021 DevPanel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation version 3 of the
+# License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# For GNU Affero General Public License see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+
+echo -e "-------------------------------"
+echo -e "| DevPanel Quickstart Creator |"
+echo -e "-------------------------------\n"
+
+echo -e "*** Framework not supported yet! ***"

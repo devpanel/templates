94d6abe4533e82bd25197fe3f188f810848a28b2
levi-devpanel <109947881+levidevpanel@users.noreply.github.com>
2025-08-20 15:07:41 +0700

    Add Laravel folder

--- laravel_v2/init.sh
+++ laravel_v2/init.sh
@@ -2,9 +2,9 @@
 # ---------------------------------------------------------------------
 # Copyright (C) 2021 DevPanel
 #
-# This program is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify it
 # it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation version 3 of the
+# published by the Free Software Foundation, version 3 of the
 # License.
 #
 # This program is distributed in the hope that it will be useful,
@@ -22,7 +22,17 @@ if [[ ! -n "$APACHE_RUN_GROUP" ]]; then
   export APACHE_RUN_GROUP=www-data
 fi
 
-#== If webRoot has not been difined, we will set appRoot to webRoot
+#== If webRoot has not been defined, we will set appRoot to webRoot
 if [[ ! -n "$WEB_ROOT" ]]; then
   export WEB_ROOT=$APP_ROOT
 fi
+cd $APP_ROOT
+composer install
+chmod +x /var/www/html/artisan
+sudo chown -R www:www /var/www/html
+sudo chmod -R 775 /var/www/html/storage
+sudo chmod -R 775 /var/www/html/bootstrap/cache
+sudo chown -R www:www /var/www/html/storage /var/www/html/bootstrap/cache
+sudo chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache
+php artisan config:clear
+php artisan migrate --force

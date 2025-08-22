9e1442b82433abbc4b2eeaa82878964d35c65455
levi-devpanel <109947881+levidevpanel@users.noreply.github.com>
2025-08-22 06:37:45 +0000

    Add Shopware folder

--- shopware_v2/init.sh
+++ shopware_v2/init.sh
@@ -2,9 +2,9 @@
 # ---------------------------------------------------------------------
 # Copyright (C) 2021 DevPanel
 #
-# This program is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify it
 # it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation version 3 of the
+# published by the Free Software Foundation, version 3 of the
 # License.
 #
 # This program is distributed in the hope that it will be useful,
@@ -22,27 +22,27 @@ if [[ ! -n "$APACHE_RUN_GROUP" ]]; then
   export APACHE_RUN_GROUP=www-data
 fi
 
-#== If webRoot has not been difined, we will set appRoot to webRoot
+#== If webRoot has not been defined, we will set appRoot to webRoot
 if [[ ! -n "$WEB_ROOT" ]]; then
   export WEB_ROOT=$APP_ROOT
 fi
 
 cd $APP_ROOT
-composer install
-mkdir files
-sudo chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP public/ files/
+cp -r $APP_ROOT/.devpanel/.gitignore $APP_ROOT/.gitignore
 
-echo ">>> Install shopware package";
-sudo -E bin/console system:install --basic-setup --force
+echo ">>> Install Dependencies";
+composer install --no-interaction --optimize-autoloader
+
+echo ">>> Install Shopware Application";
+bin/console system:install --basic-setup --force
+
+echo ">>> Add Devpanel Admin User";
+bin/console user:create devpanel --password=devpanel --email=developer@devpanel.com --firstName=DevPanel 
 
 echo ">>> allow-plugins";
 composer config --no-plugins allow-plugins.php-http/discovery true
 
-echo ">>> Install dev-tools";
-composer require --dev shopware/dev-tools
-
-echo ">>> Import database";
-bin/console framework:demodata
-bin/console dal:refresh:index
-bin/console cache:clear
+# echo ">>> Install dev-tools";
+# composer require --dev shopware/dev-tools
+# bin/console cache:clear
 echo ">>> Successful, please refresh your web page.";
